de2d480d5da505732910338d675464b8bbb9bdfd
xiewen <1123654054@qq.com>
2024-07-04 21:48:01 +0800

    前端重做，使用daisyUI以及react router

--- study-react/src/main.tsx
+++ study-react/src/main.tsx
@@ -1,27 +1,11 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./router";
 
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
-import "../app/globals.css";
-// Create a new router instance
-const router = createRouter({ routeTree });
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-// Render the app
-const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
